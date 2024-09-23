25325ba20378fba0ea87febc1c1bafb9cc2facd1
Osmangoni13 <osmanmonju@gmail.com>
2024-09-24 01:15:02 +0600

    first commit

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+
+
+const inputDonate1 = document.getElementById('input-donate1').value;
+document.getElementById('btn-donate1').addEventListener('click', function () {
+    const inputDonate1 = parseFloat(document.getElementById('input-donate1').value);
+    console.log(inputDonate1)
+    const amount1 = parseFloat(document.getElementById('amount1').innerText);
+    console.log(amount1)
+    const inputDonateNow1 = inputDonate1 + amount1;
+    document.getElementById('amount1').innerText = inputDonateNow1;
+    document.getElementById('input-donate1').value = '';
+
+    // main blance update
+
+
+})
