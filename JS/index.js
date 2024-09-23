673e45915faf47952e212b15963f781cf529804b
Osmangoni13 <osmanmonju@gmail.com>
2024-09-24 01:30:20 +0600

    first donate inppute done

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,6 @@
 
-
+const mainBlance = parseFloat(document.getElementById('main-balance').innerText);
+console.log("main balance", mainBlance)
 const inputDonate1 = document.getElementById('input-donate1').value;
 document.getElementById('btn-donate1').addEventListener('click', function () {
     const inputDonate1 = parseFloat(document.getElementById('input-donate1').value);
@@ -11,6 +12,10 @@ document.getElementById('btn-donate1').addEventListener('click', function () {
     document.getElementById('input-donate1').value = '';
 
     // main blance update
+    remainTarget = mainBlance - inputDonate1;
+    document.getElementById('main-balance').innerText = remainTarget;
+
+
 
 
 })
